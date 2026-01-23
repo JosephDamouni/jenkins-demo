fe2b6ea599c0eb4e06f76d727fe7484591a5d262
JosephDamouni <drany143@gmail.com>
2026-01-23 19:22:26 +0200

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,1 +1,17 @@
+import unittest
+from app import add, subtract
 
+class TestCalculator(unittest.TestCase):
+    
+    def test_add(self):
+        """Test addition"""
+        self.assertEqual(add(5, 3), 8)
+        self.assertEqual(add(0, 0), 0)
+    
+    def test_subtract(self):
+        """Test subtraction"""
+        self.assertEqual(subtract(10, 4), 6)
+        self.assertEqual(subtract(5, 5), 0)
+
+if __name__ == '__main__':
+    unittest.main()
