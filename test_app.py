6429edcab0a94af7c217f0974d4bbd78b39ea9c6
JosephDamouni <drany143@gmail.com>
2026-01-23 21:07:30 +0200

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -5,7 +5,7 @@ class TestCalculator(unittest.TestCase):
     
     def test_add(self):
         """Test addition"""
-        self.assertEqual(add(5, 3), 8)
+        self.assertEqual(add(5, 3), 88)
         self.assertEqual(add(0, 0), 0)
     
     def test_subtract(self):
